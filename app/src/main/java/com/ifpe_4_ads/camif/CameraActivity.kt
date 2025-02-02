44d25c0ff1054211cd5ec60c0d513b5cec51236e
Denilson <denilsongunner@gmail.com>
2025-02-01 22:22:59 -0300

    photo capture

--- app/src/main/java/com/ifpe_4_ads/camif/CameraActivity.kt
+++ app/src/main/java/com/ifpe_4_ads/camif/CameraActivity.kt
@@ -2,20 +2,34 @@ package com.ifpe_4_ads.camif
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraExecutor
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.ifpe_4_ads.camif.databinding.ActivityCameraBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
 
     private lateinit var cameraProvider: ProcessCameraProvider
     private lateinit var preview: Preview
+    private lateinit var outputDirectory: File
+    private var imageCapture: ImageCapture?=null
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +37,56 @@ class CameraActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             requestPermissions()
         }
 
+        binding.btnTake.setOnClickListener {
+            takePhoto()
+        }
+
+    }
+
+    private fun getOutputDirectory(): File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{mFile->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto(){
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(Constants.FILE_NAME_FORMAT, Locale.getDefault()).format(System.currentTimeMillis())+".jpg")
+
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOption, ContextCompat.getMainExecutor(this),
+            object :ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+
+
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Foto Salva"
+                    Toast.makeText(this@CameraActivity, "$msg $savedUri", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+                }
+
+            }
+        )
     }
 
     private fun startCamera() {
@@ -43,8 +101,10 @@ class CameraActivity : AppCompatActivity() {
         preview = Preview.Builder().build()
         preview.setSurfaceProvider(binding.previewView.surfaceProvider) // Usando o PreviewView
 
+        imageCapture = ImageCapture.Builder().build()
+
         val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
     }
 
     private fun allPermissionsGranted(): Boolean {
@@ -60,5 +120,10 @@ class CameraActivity : AppCompatActivity() {
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
---
+++ app/src/main/java/com/ifpe_4_ads/camif/Constants.kt
@@ -0,0 +1,6 @@
+package com.ifpe_4_ads.camif
+
+object Constants {
+    const val TAG  = "camexaX"
+    const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
+}
